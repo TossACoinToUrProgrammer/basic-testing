f9f7731d590ab212477ce00380e002ceafacaab4
Arlen Manasov <arlen.manasov@icloud.com>
2023-11-26 15:08:33 +0600

    completed testing task

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,45 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
     // Write your test here
+    expect(simpleCalculator({ a: 2, b: 4, action: Action.Add })).toBe(6);
   });
 
   test('should subtract two numbers', () => {
     // Write your test here
+    expect(simpleCalculator({ a: 6, b: 4, action: Action.Subtract })).toBe(2);
   });
 
   test('should multiply two numbers', () => {
     // Write your test here
+    expect(simpleCalculator({ a: 6, b: 4, action: Action.Multiply })).toBe(24);
   });
 
   test('should divide two numbers', () => {
     // Write your test here
+    expect(simpleCalculator({ a: 6, b: 2, action: Action.Divide })).toBe(3);
   });
 
   test('should exponentiate two numbers', () => {
     // Write your test here
+    expect(simpleCalculator({ a: 6, b: 2, action: Action.Exponentiate })).toBe(
+      36,
+    );
   });
 
   test('should return null for invalid action', () => {
     // Write your test here
+    expect(
+      simpleCalculator({ a: 6, b: 4, action: 'Action.Multiply' }),
+    ).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
     // Write your test here
+    expect(
+      simpleCalculator({ a: 6, b: "4", action: Action.Add }),
+    ).toBeNull();
   });
 });
--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,5 +1,8 @@
 // Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import path from 'path'
+import { existsSync } from 'fs';
+import { readFile } from 'fs/promises';
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
@@ -12,10 +15,23 @@ describe('doStuffByTimeout', () => {
 
   test('should set timeout with provided callback and timeout', () => {
     // Write your test here
+    const cb = jest.fn();
+    doStuffByTimeout(cb, 500);
+
+    jest.advanceTimersByTime(500)
+    expect(cb).toHaveBeenCalledTimes(1);
   });
 
   test('should call callback only after timeout', () => {
     // Write your test here
+    const cb = jest.fn();
+    doStuffByTimeout(cb, 500);
+
+    jest.advanceTimersByTime(200)
+    expect(cb).not.toHaveBeenCalled()
+
+    jest.advanceTimersByTime(400)
+    expect(cb).toHaveBeenCalled()
   });
 });
 
@@ -30,23 +46,55 @@ describe('doStuffByInterval', () => {
 
   test('should set interval with provided callback and timeout', () => {
     // Write your test here
+    const cb = jest.fn();
+    doStuffByInterval(cb, 150);
+
+    jest.advanceTimersByTime(400)
+    expect(cb).toHaveBeenCalledTimes(2)
   });
 
   test('should call callback multiple times after multiple intervals', () => {
     // Write your test here
+    const cb = jest.fn();
+    doStuffByInterval(cb, 150);
+
+    jest.advanceTimersByTime(300)
+    expect(cb).toHaveBeenCalledTimes(2)
+    jest.advanceTimersByTime(150)
+    expect(cb).toHaveBeenCalledTimes(3)
   });
 });
+
+jest.mock('path');
+jest.mock('fs/promises');
+jest.mock('fs');
 
 describe('readFileAsynchronously', () => {
   test('should call join with pathToFile', async () => {
     // Write your test here
+    const pathToFile = "./index.ts"
+    await readFileAsynchronously(pathToFile)
+
+    expect(path.join).toHaveBeenCalledWith(__dirname, pathToFile)
   });
 
   test('should return null if file does not exist', async () => {
     // Write your test here
+    const filename = 'notFoundFile';
+
+    // Act
+    const result = await readFileAsynchronously(filename)
+    expect(result).toBeNull();
   });
 
   test('should return file content if file exists', async () => {
     // Write your test here
+    const filename = 'existing.txt';
+    const content = 'This is the file content';
+    (existsSync as jest.Mock).mockReturnValue(true);
+    (readFile as jest.Mock).mockResolvedValue(content);
+
+    const result = await readFileAsynchronously(filename)
+    expect(result).toBe(content);
   });
 });
